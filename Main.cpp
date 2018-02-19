ed51016adcbca7d1e92883ef30b8cdae05f47044
Chanwit Piromplad <kingkong2103@gmail.com>
2018-02-19 23:48:52 +0700

    edit format

--- Main.cpp
+++ Main.cpp
@@ -12,166 +12,181 @@ void find();
 void remove();
 
 ////////////////////////////// MAIN //////////////////////////////
-
-int main(){
+int main() {
 	
 	string choice;
 	bool stop = true;
 	
-	
-	do{
-	system("cls");
-	cout<<"/////// BINARY SEARCH TREE \\\\\\\\\\\\ \n\n";
-	cout<<"\t1.) Insert\n\t2.) Print\n\t3.) Find\n\t4.) Remove\n\t5.) Quit\n\n";
-	cout<<"Enter Choice : ";
-	cin>>choice;
-
-	switch(choice[0]){
-	case '1':
-		insert();
-	break;
-	case '2':
-		print();
-	break;
-	case '3':
-		find();
-	break;
-	case '4':
-		remove();
-	break;
-	case '5' :
+	do {
 		system("cls");
-		stop = false;
-		break;
-	default :
-		cout<<"\n!!! Wrong choice Please Try Again !!! \n\n";
-		system("pause");
-	break;
-	}
-	}while(stop);
+		cout<<"/////// Assignment2:BST \\\\\\\\\\\\\\ \n\n";
+		cout<<"\t1.Insert\n";
+		cout<<"\t2.PrintTree\n";
+		cout<<"\t3.Find\n";
+		cout<<"\t4.Remove\n";
+		cout<<"\t5.Quit\n\n";
+		cout<<"Enter Choice: ";
+		cin>>choice;
 
+		switch(choice[0]) {
+			case '1': {
+				insert();
+				break;
+			}
+			case '2': {
+				print();
+				break;
+			}
+			case '3': {
+				find();
+				break;
+			}
+			case '4': {
+				remove();
+				break;
+			}
+			case '5': {
+				system("cls");
+				stop = false;
+				break;
+			}
+			default: {
+				cout<<"\n!!! Wrong choice. Please Try Again !!! \n\n";
+				system("pause");
+				break;
+			}
+		}
+	} while(stop);
 	return 0;
 }
 
 ////////////////////////////// INSERT ///////////////////////////////
 
-void insert(){
-	int x;
-	bool stop=true;
-	do{
-	system("cls");
-	cout<<"======= INSERT MODE ======\n";
-	cout<<"\nEnter element for insert to Tree : ";
-	cin>>x;
-	if(t.find(x)==-999)
-	{
-		t.insert(x);
-		stop=false;
-		cout<<"\n!!! Insert Element Successful. !!!\n"<<endl;
+void insert() {
+	int element;
+	bool stop = true;
 
-	}
-	else 
-		cout<<"\nThis element has in Tree. \n!!! Please Try Again. !!!\n\n";
-		system("pause");
-	}while(stop);
+	do {
+		system("cls");
+		cout<<"======= INSERT MODE ======\n";
+		cout<<"\nEnter Element: ";
+		cin>>element;
 		
+		if (t.find(element) == -999) {
+			t.insert(element);
+			stop = false;
+			cout<<"\nInsert Element Successful. !!!\n"<<endl;
+		} else {
+			cout<<"\nThis element has in Tree. \n!!! Please Try Again. !!!\n\n";
+			system("pause");
+		}
+	} while(stop);
 }
 
 ////////////////////////////// PRINT ////////////////////////////////
 
-void print(){
+void print() {
 	string choice;
-	bool stop=true;
+	bool stop = true;
 
-	do{
-	system("cls");
-	cout<<"====== PRINT MODE ======\n\n";
-	cout<<"\t1.) Pre Order\n\t2.) In Order\n\t3.) Post Order\n\t4.) Back\n\n";
-	cout<<"Enter Choice : ";
-	cin>>choice;
-	cout<<endl;
+	do {
+		system("cls");
+		cout<<"====== PRINT MODE ======\n\n";
+		cout<<"\t1.Pre Order\n";
+		cout<<"\t2.In Order\n";
+		cout<<"\t3.Post Order\n";
+		cout<<"\t4.Back\n\n";
+		cout<<"Enter Choice: ";
+		cin>>choice;
+		cout<<endl;
 
-	if(choice[0]>'0'&&choice[0]<'4')
-	{
-		t.printTree(choice[0]);
-		cout<<endl<<endl;
-		system("pause");
-	}
-	else if(choice[0]=='4') 
-		stop=false;
-	else 
-	{
-		cout<<"!!! Wrong choice Please Try Again !!! \n\n";
-		system("pause");
-	}
-	}while(stop);
+		if(choice[0] > '0' && choice[0] < '4') {
+			t.printTree(choice[0]);
+			cout<<endl<<endl;
+			system("pause");
+		} else if(choice[0]=='4') {
+			stop=false;
+		} else {
+			cout<<"!!! Wrong choice Please Try Again !!! \n\n";
+			system("pause");
+		}
+	} while(stop);
 }
 
 ////////////////////////////// FIND /////////////////////////////////
 
-void find(){
+void find() {
 	string choice;
-	int x;
-	bool stop= true;
-	do{
-	system("cls");
-	cout<<"====== FIND MODE ======\n\n";
-	cout<<"\t1.) Find Element\n\t2.) Find Min\n\t3.) Find Max\n\t4.) Back\n\n";
-	cout<<"Enter Choice : ";
-	cin>>choice;
-	switch(choice[0]){
-	case '1' :
-		system("cls");
-		cout<<" ====== FIND ELEMENT MODE ======\n\n";
-		cout<<"Enter element for Find in Tree : ";
-			cin>>x;
-			if(t.findChild(x)==-999){
-			cout<<"\n!!! CHILD NOT FOUND !!!\n\n";
-			}
-			system("pause");
-		break;
-	case '2' :
-		system("cls");
-		cout<<" ====== FIND MIN MODE ======\n\n";
-		t.findMin();
-		system("pause");
-		break;
-	case '3' :
-		system("cls");
-		cout<<" ====== FIND MAX MODE ======\n\n";
-		t.findMax();
-		system("pause");
-		break;
-	case '4' :
-		stop=false;
-		break;
-	default :
-		cout<<"!!! Wrong choice Please Try Again !!! \n\n";
-		system("pause");
-	break;
-	}
+	int element;
+	bool stop = true;
 
-	}while(stop);
+	do {
+
+		system("cls");
+		cout<<"====== FIND MODE ======\n\n";
+		cout<<"\t1.Find Element\n";
+		cout<<"\t2.Find Min\n";
+		cout<<"\t3.Find Max\n";
+		cout<<"\t4.Back\n\n";
+		cout<<"Enter Choice : ";
+		cin>>choice;
+
+		switch(choice[0]) {
+			case '1': {
+				system("cls");
+				cout<<" ====== FIND ELEMENT MODE ======\n\n";
+				cout<<"Enter element for Find in Tree : ";
+				cin>>element;
+
+				if(t.findChild(element) == -999) {
+					cout<<"\n!!! CHILD NOT FOUND !!!\n\n";
+				}
+				system("pause");
+				break;
+			}
+			case '2': {
+				system("cls");
+				cout<<" ====== FIND MIN MODE ======\n\n";
+				t.findMin();
+				system("pause");
+				break;
+			}
+			case '3': {
+				system("cls");
+				cout<<" ====== FIND MAX MODE ======\n\n";
+				t.findMax();
+				system("pause");
+				break;
+			}
+			case '4': {
+				stop=false;
+				break;
+			}
+			default: {
+				cout<<"!!! Wrong choice Please Try Again !!! \n\n";
+				system("pause");
+				break;
+			}
+		}
+	} while(stop);
 }
 
 ////////////////////////////// REMOVE ///////////////////////////////
 
-void remove(){
+void remove() {
 
-	int x;
+	int element;
 	
 	system("cls");
 	cout<<"======= REMOVE MODE ======\n";
-	cout<<"\nEnter element for Remove in Tree : ";
-	cin>>x;
-	if(t.find(x)!=-999)
-	{
-		t.remove(x);
-		cout<<"\n!!! Remove Element Successful. !!!\n"<<endl;
-
-	}
-	else 
-		cout<<"\nThis element haven't in Tree. \n!!! Please Try Again. !!!\n\n";
+	cout<<"\nEnter Element: ";
+	cin>>element;
+	if(t.find(element) != -999) {
+		t.remove(element);
+		cout<<"\n!!! Remove Element Successful. !!!\n\n";
+	} else {
+		cout<<"\nThis element haven't in Tree. \n";
+		cout<<"!!! Please Try Again. !!!\n\n";
 		system("pause");
-	
+	}
 }
